d270ba01efe8a203b71d9cb8ffc7b274ee67c947
Weldar <34899832+Weldar@users.noreply.github.com>
2018-06-18 08:32:22 -0400

    Upload ino

---
+++ mouse_clicker.ino
@@ -0,0 +1,28 @@
+#include <Mouse.h>
+
+/* 
+ *  Name: Browser Timeout Clicker
+ *  Date: 06-18-2018
+ *  Desc: An Arduino based mouse emulator, designed to keep a browser window from timing out by showing some activity.
+ *  Author: Weldar https://gitlab.com/weldar
+ *  Version 1.0
+ */
+
+void setup() {
+  // First time setup code, NEVER looped.
+
+   Mouse.begin();
+}
+
+void loop() {
+  // Loop that clicks the left mouse button twice every 30 seconds.
+  delay(1000);
+
+  while(true) {
+    Mouse.click(MOUSE_LEFT);
+    delay(500);
+    Mouse.click(MOUSE_LEFT);
+
+    delay(30000);
+  }
+}
